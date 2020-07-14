70f1258a719845cfc9fd09077c2c06fe8d4fd54c
Aditya Sinha <sinha.aditya02@gmail.com>
2020-07-15 05:27:40 +0530

    Initial Commit

---
+++ LocationSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationSearch
+//
+//  Created by Aditya Sinha on 15/07/20.
+//  Copyright © 2020 codewalla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
