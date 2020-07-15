23eceab56eb9e26cad6c939679f865f004856b9b
Aditya Sinha <sinha.aditya02@gmail.com>
2020-07-15 07:04:35 +0530

    initial commit for testing location search feature.

--- LocationSearch/ViewController.swift
+++ LocationSearch/ViewController.swift
@@ -7,14 +7,122 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchControllerView: UIView!
+    @IBOutlet weak var locationsTableView: UITableView!
+    
+    var searchController: UISearchController!
+    
+    let locationManager = CLLocationManager()
+    
+    var matchingItems:[MKMapItem] = []
+    var userRegion: MKCoordinateRegion?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLocationManager()
+        setupSearchController()
+        locationsTableView.dataSource = self
+        locationsTableView.delegate = self
+        
+    }
+    
+    func setupLocationManager() {
+        
+        locationManager.delegate = self
+        //locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    func setupSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchControllerView.addSubview(searchController.searchBar)
     }
 
 
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            print(region.center)
+            userRegion = region
+        }
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("inside error: \(error.localizedDescription)")
+    }
+}
+
+extension ViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchBarText = searchController.searchBar.text, let usrRegion = userRegion else { return }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBarText
+        request.region = usrRegion
+        
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.locationsTableView.reloadData()
+        }
+        
+    }
+    
+    
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("Cancel pressed")
+        matchingItems = []
+        locationsTableView.reloadData()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = ""
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let location = matchingItems[indexPath.row].placemark
+        searchController.searchBar.text = location.name
+    }
 }
 
